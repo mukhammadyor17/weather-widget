0816cb15a9b8a6ea7345114b331e2e032d89b5e4
mukhammadyor17 <mukhammadyorkhon.t@evrika.com>
2025-11-22 11:35:56 +0500

    feat: add draggable

--- src/views/settings_view/SettingsPage.vue
+++ src/views/settings_view/SettingsPage.vue
@@ -7,7 +7,7 @@
   </page-header>
 
   <div class="mt-4">
-    <div class="mb-3 flex gap-4">
+    <div class="mb-5 flex gap-4">
       <input
         v-model.trim="cityName"
         @keyup.enter="addNewCity"
@@ -28,28 +28,37 @@
 
     <p v-if="alreadyExists" class="text-red-500 text-xs mb-4">This city is already added.</p>
 
-    <div class="flex flex-col divide-y divide-gray-200">
-      <div v-for="city in cityStore.cities" :key="city" class="flex items-center gap-4 py-2">
-        <menu-icon class="opacity-40" />
-
-        <div class="flex-1 capitalize">
-          {{ city }}
+    <draggable-component
+      v-model="cityStore.cities"
+      group="cities"
+      animation="200"
+      item-key="element"
+      handle=".drag-handle"
+    >
+      <template #item="{ element }">
+        <div class="flex items-center gap-4 py-2 border-t border-gray-200">
+          <button class="drag-handle cursor-grab active:cursor-grabbing opacity-40">
+            <menu-icon />
+          </button>
+          <div class="flex-1 capitalize select-none">
+            {{ element }}
+          </div>
+          <button
+            class="cursor-pointer text-red-500 hover:text-red-700 transition"
+            @click="removeCity(element)"
+          >
+            <trash-icon />
+          </button>
         </div>
-
-        <button
-          class="cursor-pointer text-red-500 hover:text-red-700 transition"
-          @click="removeCity(city)"
-        >
-          <trash-icon />
-        </button>
-      </div>
-    </div>
+      </template>
+    </draggable-component>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 import { useCityStore } from '@/stores/city_store'
+import draggableComponent from 'vuedraggable'
 import CloseIcon from '@/components/icons/CloseIcon.vue'
 import TrashIcon from '@/components/icons/TrashIcon.vue'
 import MenuIcon from '@/components/icons/MenuIcon.vue'
